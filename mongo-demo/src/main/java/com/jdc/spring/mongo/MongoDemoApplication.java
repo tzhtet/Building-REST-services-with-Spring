e52059be3773b2dd0ae8105ee416e6dd5f18bac5
thanzawhtet <thanzawhtet18@gmail.com>
2025-01-22 17:23:49 +0630

    test-demo

--- mongo-demo/src/main/java/com/jdc/spring/mongo/MongoDemoApplication.java
+++ mongo-demo/src/main/java/com/jdc/spring/mongo/MongoDemoApplication.java
@@ -1,13 +1,31 @@
 package com.jdc.spring.mongo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jdc.spring.mongo.product.Product;
+import com.jdc.spring.mongo.product.ProductRepository;
 
 @SpringBootApplication
 public class MongoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoDemoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			ProductRepository repository
+			) {
+		return args -> {
+			var product = Product.builder()
+					.name("iPhone")
+					.description("Smart Phone")
+					.build();
+			repository.insert(product);
+		};
 	}
 
 }
--- mongo-demo/src/main/java/com/jdc/spring/mongo/product/Product.java
+++ mongo-demo/src/main/java/com/jdc/spring/mongo/product/Product.java
@@ -1,5 +1,6 @@
 package com.jdc.spring.mongo.product;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Data;
 @Builder
 public class Product {
 	
+	@Id
 	private String id;
 	
 	private String name;
