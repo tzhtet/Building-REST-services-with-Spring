f9ee705583337949ad9cfe6b684ae6f7b1cda324
thanzawhtet <thanzawhtet18@gmail.com>
2025-01-23 14:51:49 +0630

    today-test

--- mongo-demo/src/main/java/com/jdc/spring/mongo/MongoDemoApplication.java
+++ mongo-demo/src/main/java/com/jdc/spring/mongo/MongoDemoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.jdc.spring.mongo.product.Product;
 import com.jdc.spring.mongo.product.ProductRepository;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.jdc.spring.mongo.product")
 public class MongoDemoApplication {
 
 	public static void main(String[] args) {
@@ -16,16 +18,16 @@ public class MongoDemoApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner commandLineRunner(
-			ProductRepository repository
-			) {
-		return args -> {
-			var product = Product.builder()
-					.name("iPhone")
-					.description("Smart Phone")
-					.build();
-			repository.insert(product);
-		};
+	public CommandLineRunner commandLineRunner(ProductRepository repository) {
+	    return args -> {
+	        System.out.println("CommandLineRunner is executing...");
+	        var product = Product.builder()
+	                .name("iPhone")
+	                .description("Smart Phone")
+	                .build();
+	        repository.insert(product);
+	        System.out.println("Product inserted successfully.");
+	    };
 	}
 
 }
