620d8a4d1f047c7426d3f13aa0b4c540b3153184
thanzawhtet <thanzawhtet18@gmail.com>
2025-01-24 13:53:41 +0630

    update

--- balance-api/src/main/java/com/jdc/spring/model/LoginUser.java
+++ balance-api/src/main/java/com/jdc/spring/model/LoginUser.java
@@ -1,12 +1,19 @@
 package com.jdc.spring.model;
 
+import com.jdc.spring.model.entity.Account;
+
 public record LoginUser(
 		String loginId,
 		String name,
 		Role role
 		) {
 	
-	
+	public static LoginUser from(Account account) {
+		return new LoginUser(
+				account.getLoginId(),
+				account.getName(),
+				account.getRole());
+	}
 	
 	
 }
