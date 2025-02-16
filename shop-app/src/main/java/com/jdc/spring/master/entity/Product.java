9ec8d5d2adcce357ae33cf10fbb239cf6ccc75e9
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-02-16 20:52:19 +0630

    update-code

--- shop-app/src/main/java/com/jdc/spring/master/entity/Product.java
+++ shop-app/src/main/java/com/jdc/spring/master/entity/Product.java
@@ -1,5 +1,54 @@
 package com.jdc.spring.master.entity;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(indexes = {
+		@Index(columnList = "category_id,name",unique = true)
+})
+@EqualsAndHashCode(callSuper = false)
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne(mappedBy = "product")
+	private ProductStock stock;
+	
+	@ManyToOne(optional = false)
+	private Category category;
+	
+	@Column(nullable = false)
+	private String name;
+	private int salePrice;
+	
+	@Column(columnDefinition = "Text")
+	private String image;
+	private String description;
+	
+	@ElementCollection
+	@MapKeyColumn(name = "property")
+	@CollectionTable(name = "product_properties")
+	private Map<String, String> properties = new HashMap<>();
+	
+	private List<ProductStockHistory> stockHistory;
 }
--- shop-app/src/main/java/com/jdc/spring/master/entity/ProductStockHistoryPk.java
+++ shop-app/src/main/java/com/jdc/spring/master/entity/ProductStockHistoryPk.java
@@ -1,5 +1,31 @@
 package com.jdc.spring.master.entity;
 
-public class ProductStockHistoryPk {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductStockHistoryPk implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private static final DateTimeFormatter DF = DateTimeFormatter.ofPattern("yyyyMMdd");
+	
+	@Column(name = "issue_at")
+	private LocalDate issueAt;
+	
+	@Column(name = "product_id")
+	private int productId;
+	
+	private int seqNumber;
 
 }
