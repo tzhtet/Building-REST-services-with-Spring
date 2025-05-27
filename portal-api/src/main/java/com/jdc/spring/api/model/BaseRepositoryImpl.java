36a464e7f086146201b5fed72a7970b6ce419eb0
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-05-27 17:28:07 +0630

    update

--- portal-api/src/main/java/com/jdc/spring/api/model/BaseRepositoryImpl.java
+++ portal-api/src/main/java/com/jdc/spring/api/model/BaseRepositoryImpl.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import jakarta.persistence.EntityManager;
 
 public class BaseRepositoryImpl<T,ID> extends SimpleJpaRepository<T, ID> implements BaseRepository<T, ID> {
-
+ 
+	private EntityManager em;
+	
 	public BaseRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
 		super(entityInformation, entityManager);
 	}
